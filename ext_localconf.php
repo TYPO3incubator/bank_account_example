4599391441f4f4a66a3c9f1567b96ce2c5e1d283
Oliver Hader <oliver@typo3.org>
2016-08-17 20:15:37 +0200

    [TASK] Add plugin configuration

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,2 +1,9 @@
 <?php
 defined('TYPO3_MODE') or die();
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'H4ck3r31.BankAccountExample',
+    'Management',
+    ['Account' => 'list, new, create, edit, update, delete'],
+    ['Account' => 'list, new, create, edit, update, delete']
+);
--- ext_tables.php
+++ ext_tables.php
@@ -6,6 +6,11 @@ defined('TYPO3_MODE') or die();
     'Configuration/TypoScript',
     'BankAccountExample'
 );
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'H4ck3r31.BankAccountExample',
+    'Management',
+    'Bank Account Management'
+);
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
     'tx_bankaccountexample_domain_model_account'
 );
