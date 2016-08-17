9b4b9270758b83718f8b6063680bdbcd235b74e6
Oliver Hader <oliver@typo3.org>
2016-08-17 20:12:56 +0200

    [TASK] Add CommandManager

---
+++ Classes/Domain/AccountCommandManager.php
@@ -0,0 +1,66 @@
+<?php
+namespace H4ck3r31\BankAccountExample\Domain;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use H4ck3r31\BankAccountExample\Common;
+use H4ck3r31\BankAccountExample\Domain\Command;
+use H4ck3r31\BankAccountExample\Domain\Model\Account;
+use H4ck3r31\BankAccountExample\Domain\Repository\AccountRepository;
+use TYPO3\CMS\DataHandling\Core\Object\Instantiable;
+use TYPO3\CMS\DataHandling\Core\Utility\ClassNamingUtility;
+
+/**
+ * DepositCommand
+ */
+class AccountCommandManager implements Instantiable
+{
+    /**
+     * @return AccountCommandManager
+     */
+    public static function instance()
+    {
+        return Common::getObjectManager()->get(AccountCommandManager::class);
+    }
+
+    /**
+     * @param Command\AbstractCommand $command
+     * @return AccountCommandManager
+     */
+    public function manage(Command\AbstractCommand $command)
+    {
+        $commandName = ClassNamingUtility::getLastPart($command);
+        $commandCallable = array($this, 'manage' . $commandName);
+
+        if (is_callable($commandCallable)) {
+            call_user_func($commandCallable, $command);
+        }
+
+        return $this;
+    }
+
+    protected function manageCreateCommand(Command\CreateCommand $command)
+    {
+        AccountRepository::instance()->addEvents(
+            Account::create($command->getHolder(), $command->getNumber())->getEvents()
+        );
+    }
+
+    protected function manageCreateChainedCommand(Command\CreateChainedCommand $command)
+    {
+        AccountRepository::instance()->addEvents(
+            Account::create()->getEvents()
+        );
+    }
+}
